3f3ba11251fbfc4df084e0a43a22295fe45d9350
Magialis <lmagiac@gmail.com>
2025-02-19 15:37:46 -0300

    commit inicial

---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react"
+
+const Formulario = (props) => {
+    const [materiaA, setMateriaA] = useState(0); // retorna o valor e uma função para alterar esse valor
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState('');
+
+    useEffect( () => {
+        console.log("o componente iniciou");
+
+        return () => {
+            console.log("o componente finalizou")
+        }
+    }, [props.nome]);
+
+    const alteraNome = (evento) => {
+        // podemos utilizar o setNome tanto assim:
+        // setNome(evento.target.value);
+
+        // quanto acessar o valor antigo: 
+        setNome(estadoAnterior => {
+            return evento.target.value;
+        })
+    }
+
+
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC
+        const media = soma / 3
+
+        if (media >= 7) {
+            return (
+                <p>Olá {nome}, você foi aprovado</p>
+            )
+        } else {
+            return (
+                <p>Olá {nome}, você não foi aprovado</p>
+            )
+        }
+    }
+
+    return (
+        <form>
+            
+            <ul>
+                {[1, 2, 3, 4, 5].map(item => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+            
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder="Nota matéria A" onChange={({ target }) => setMateriaA(parseInt(target.value))} />
+            <input type="number" placeholder="Nota matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))} />
+            <input type="number" placeholder="Nota matéria C" onChange={evento => setMateriaC(parseInt(evento.target.value))} />
+            {renderizaResultado()}
+        </form>
+    )
+}
+
+export default Formulario
